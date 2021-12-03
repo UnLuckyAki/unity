839b6ff1d6f62cf9f6f6ea6fd17ca5e30458b427
UnLuckyAki <aristocrat98@mail.ru>
2021-12-03 10:19:46 +1000

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+
+    public float runSpeed = 500f;
+    public float strafeSpeed = 500f;
+    public float jumpForce = 15f;
+
+    protected bool strafeLeft = false;
+    protected bool strafeRight = false;
+    protected bool doJump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("a"))
+        {
+            strafeLeft = true;
+        } else
+        {
+            strafeLeft = false;
+        }
+
+        if (Input.GetKey("d"))
+        {
+            strafeRight = true;
+        }
+        else
+        {
+            strafeRight = false;
+        }
+        if(Input.GetKeyDown("space"))
+        {
+            doJump = true;
+        }
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + Vector3.forward * runSpeed * Time.deltaTime);
+        if(strafeLeft)
+        {
+            rb.AddForce(-strafeSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (strafeRight)
+        {
+            rb.AddForce(strafeSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if(doJump)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+            doJump = false;
+        }
+    }
+}
